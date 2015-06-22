0e4f21d4679004f6ff5cc3cce41a53765f62cfc8
jacormenzana <jacormenzana@gmail.com>
2015-06-22 20:48:35 +0200

    Quiz 1b: El esqueleto del proyecto

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//APP.JS: Primero-> Importa primero los MWs que va a instalar con require(….).
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,14 +7,25 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//APP.JS: Segundo-> Importa enrutadores
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+//APP.JS: Tercero-> Crea aplicación
 var app = express();
 
+
+//APP.JS: Cuarto-> Instala el generador de vistas ejs
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+
+//APP.JS: Quinto-> Instala MWs con app.use().
+//                  Los MWs se instalan en el orden en que deben ejecutarse
+//                  cuando llegue una transacción HTTP.Cada transacción HTTP 
+//                  ejecuta los MWs instalados en orden en que han sido instalados.
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,20 +35,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//APP.JS: Sexto-> Instala enrutadores
+//                Asocia rutas con sus gestores
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+
+//APP.JS: Séptimo-> Se crean e instalan tres MWs
+
+//                  MW que atiende a rutas no atendidas por 
+//                  rutas anteriores. Se genera error HTTP 404 
+//                  catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+//                  MW gestor de errores para la fase de
+//                  desarrollo del proyecto. 
+//                  error handler will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +66,9 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+//                  MW gestor de errores para la fase de
+//                  producción del proyecto. 
+//                  no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -56,5 +77,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+// APP.JS: Octavo-> Exporta app para comando de arranque
 module.exports = app;
