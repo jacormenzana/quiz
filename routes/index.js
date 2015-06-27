9e04a1da6862ab46697772698e0408d032373986
jacormenzana <jacormenzana@gmail.com>
2015-06-27 22:43:28 +0200

    Quiz9: Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+ //GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
-router.get('/author',quizController.author)
+
+router.get('/quizes'						,quizController.index);
+router.get('/quizes/:quizId(\\d+)'			,quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer'	,quizController.answer);
+router.get('/author'						,quizController.author);
 module.exports = router;
