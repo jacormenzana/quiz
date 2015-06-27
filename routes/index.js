735e96521b72ae5ca953085ad27411e20f4a9625
jacormenzana <jacormenzana@gmail.com>
2015-06-27 23:09:44 +0200

    Quiz10: Autoload

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 
+
+//Definición de rutas de /quizes
 router.get('/quizes'						,quizController.index);
 router.get('/quizes/:quizId(\\d+)'			,quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer'	,quizController.answer);
