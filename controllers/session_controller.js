675330269781f471a0469e706c8b03e05cfb0464
jacormenzana <jacormenzana@gmail.com>
2015-07-07 19:58:52 +0200

    Quiz17: Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,15 @@
+//MW de autorización de accesps HTTP restringidos
+
+exports.loginRequired = function(req, res, next){
+
+	if(req.session.user) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
+
 
 // GET /login  -- Formulario de login
 exports.new = function(req, res) {
